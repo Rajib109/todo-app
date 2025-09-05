b8e0cec051ae1af22d4be610604c2676ea564e1f
Shujaat Hussain <sujathussain426@gmail.com>
2025-09-05 23:18:27 +0530

    Setting up backend

---
+++ Backend/src/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+import todoRoutes from "./routes/todoRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+// Connect to DB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/todos", todoRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Todo API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
