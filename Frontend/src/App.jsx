20558ccf60bda7e2121de9d41091067224077bb6
Shujaat Hussain <sujathussain426@gmail.com>
2025-09-05 23:02:32 +0530

    backend setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Welcome to the Todo App</h1>
+      <h1 className='text-3xl font-bold bg-blue-900'>Welcome to the Todo App</h1>
     </div>
   );
 }
