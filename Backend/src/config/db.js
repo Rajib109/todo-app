bcba5a4a934b47885a7ff1b9ec92f43583cb5248
Shujaat Hussain <sujathussain426@gmail.com>
2025-10-08 10:01:26 +0530

    fix: update MongoDB connection URI and upgrade dependencies in package-lock.json

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, { dbName: "todoApp" });
+    await mongoose.connect(process.env.MONGODB_URI, { dbName: "todoApp" });
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
